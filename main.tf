de67d0a186e7279ab73f8da512827b8afca82ff9
aisvarya2 <aisvaryaviji@gmail.com>
2025-03-16 19:25:03 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.91.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "random_string" "this" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = format("-", [random_string.this.id, var.bucket_name])
+
+  tags = {
+    Name        = "Home assignment - S3 bucket"
+    Environment = "Dev"
+  }
+}
+
+# bucket ACLs 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable server side encryption
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+# Bucket versioning
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+
+# s3 bucket policy
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2025-03-16"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.this.arn}/*"
+      }
+    ]
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  default = "assignment-s3"
+  type    = string
+}
