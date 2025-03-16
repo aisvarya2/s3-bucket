2e29e2898d199581557b2d56588afca778d303f3
aisvarya2 <aisvaryaviji@gmail.com>
2025-03-16 19:33:05 +0530

    update policy version

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
 
   policy = jsonencode({
-    Version = "2025-03-16"
+    Version = "2012-10-17"
     Statement = [
       {
         Effect    = "Allow"
